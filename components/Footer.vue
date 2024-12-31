a3e0e9e3b1fd7835956e4f81af56068fa76b2e89
ElvanVans24 <elvanvans24@gmail.com>
2024-12-31 13:56:10 +0700

    Add files via upload

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer>
+      <div class="text-white p-3 text-center bg-primary" style="height: 50px;">
+        Created by ElvanMuhammadFauzan | 2025 All Right Reserved
+      </div>
+    </footer>
+</template>
---
+++ pages/Frofil/index.vue
@@ -0,0 +1,102 @@
+<template>
+    <div id="carouselExampleCaptions" class="carousel slide bg-dark">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/img/peresmian.jpg" class="d-block w-100" style="height: 70vh;" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>lapangan smkn 4 tasikmalaya</h5>
+                </div>
+            </div>
+            <div class="carousel-item active">
+                <img src="/img/upacara.jpg" class="d-block w-100" style="height: 70vh;" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>lapangan smkn 4 tasikmalaya</h5>
+                </div>
+            </div>
+            <div class="carousel-item active">
+                <img src="/img/fropil.jpg" class="d-block w-100" style="height: 70vh;" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        
+    <div class="container-fluid bg-secondary bg-opacity-25 py-3">
+        <div class="container">
+            <div class="row py-2">
+                <h1 class="text-primary text-center mb-1">Tentang Kami</h1>
+                <p class="fs-5 mb-5 pt-3">Sejalan dengan Program Pemerintah dibidang pendidikan Menengah Kejuruan pada
+                    saat itu yakni pemerataan akses ditambah pula dengan banyaknya keinginan masyarakat yang
+                    mengharapkan adanya SMK Negeri di daerah Kecamatan Purbaratu Kota Tasikmalaya, terutama untuk
+                    menampung tamatan dari SLTP yang ingin melanjutkan ke SMK maka beberapa tokoh masyarakat, unsur
+                    pejabat pemerintah di Kecamatan Purbaratu Kota Tasikmalaya mengusulkan kepada pemerintah Kota
+                    Tasikmalaya dan Pemerintah Provinsi Jawa Barat, agar berdirinya SMK Negeri di Kecamatan Purbaratu
+                    Kota Tasikmalaya.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-6 pt-5">
+                <img src="/img/bapa.png" class="w-50 rounded-5" alt="" srcset="">
+            </div>
+            <div class="col-6 px-5">
+                <h1 class="text-center mb-5">Visi & Misi</h1>
+
+                <h3 class="text-primary">Visi</h3>
+                <ul>
+                    <li type="font-size:1em">TERWUJUDNYA LULUSAN YANG CERDAS, AKTIF, KOMPETITIF, ADAPTIF, DAN PRODUKTIF
+                        BERLANDASKAN IMAN DAN TAKWA
+
+                        (CAKAP BERIMTAK)</li>
+                </ul>
+
+                <h3 class="text-primary">Misi</h3>
+                <ul>
+                    <li type="font-size:1em">jMenyiapkan Sumber Daya Manusia yang cerdas dan kompeten baik hardskill maupun
+                        softskill</li>
+                    <li type="font-size:1em">Meningkatkan dan mengembangkan aktifitas serta kreatifitas seluruh warga sekolah
+                        dalam berbagai kegiatan positif.
+                    </li>
+                    <li type="font-size:1em">Mewujudkan manajemen pengelolaan yang efektif, efesien, transparan, akuntabel dan
+                        layanan prima.</li>
+                    <li type="font-size:1em">Menumbuhkan dan mengembangkan potensi dan kemampuan Sumber Daya Manusia yang
+                        berdaya saing tinggi melalui berbagai kegiatan akademik maupun non akademik.
+                    </li>
+                    <li type="font-size:1em">Menyelenggarakan berbagai program kegiatan dalam upaya meningkatkan Sumber Daya
+                        Manusia yang mampu menyesuaikan dengan perkembangan.
+                    </li>
+                    <li type="font-size:1em">Mengembangkan sarana prasarana dan lingkungan sekolah yang menyenangkan sebagai
+                        wadah menumbuhkembangkan daya kreasi dan inovasi untuk menghasilkan produk teknologi tepat guna.
+                    </li>
+                    <li type="font-size:1em">Membangun jiwa wirausaha yang handal melalui pembelajaran Teaching Factory (TEFA)
+                        dan Kelas Industri.</li>
+                    <li type="font-size:1em">Menjalin dan mengembangkan kemitraan dengan Industri dan Dunia Kerja (IDUKA) serta
+                        lembaga lainnya yang relevan</li>
+                    <li type="font-size:1em">Menanamkan dan membudayakan sikap dan perilaku yang baik pada aktivitas di sekolah
+                        maupun dalam kehidupan sehari-hari.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
